e348994eb5a4c90aabce5d8a757d1cf1248bfc7f
Antonio Pierro <antonio.pierro@gmail.com>
2022-01-13 15:08:07 +0100

    feat: add redirects

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,8 @@
 const { headers } = require('./security-headers.next.config');
+const { redirects } = require('./redirects.next.config');
 
 module.exports = {
     reactStrictMode: true,
+    redirects,
     headers,
 };
